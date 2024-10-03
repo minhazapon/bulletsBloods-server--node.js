956b9b14c92ca1af883faefe82efe4293a9c41d6
minhazapon <minhajulabedinapon@gmail.com>
2024-10-03 02:41:30 -0700

    first commit

---
+++ d.js
@@ -0,0 +1,24 @@
+
+[
+{
+    "image": "https://img.freepik.com/free-photo/3d-view-powerful-gun_23-2150888684.jpg?t=st=1727945755~exp=1727949355~hmac=fc0e48579aba4a55cabf7fd148dfc1f8e2fef87ef46f210a244bdc8096f807c8&w=826",
+    "title": "F-REX",
+    "details": "Guns are often seen as instruments of death, and gun control is necessary to prevent more harm."
+
+},
+{
+    "image":"https://img.freepik.com/premium-photo/cinematic-photography-gun-back-time_950002-43147.jpg?w=826",
+    "title": "K-REX",
+    "details": "Guns are often seen as instruments of death, and gun control is necessary to prevent more harm."
+
+},
+{
+    "image": "https://img.freepik.com/free-photo/close-up-decorated-gun-black-background_23-2150803692.jpg?t=st=1727945912~exp=1727949512~hmac=b82c1f938774205f351aa055300212ebf27b43f66b23e2450e6ac103d1a21de6&w=740",
+    "title":  "T-REX",
+    "details": "Guns are often seen as instruments of death, and gun control is necessary to prevent more harm."
+
+}
+
+
+
+]
---
+++ index.js
@@ -0,0 +1,87 @@
+
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
+
+
+
+console.log(process.env.DB_USERS)
+console.log(process.env.DB_PASS)
+
+app.use(cors())
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+  res.send(' Bullet server !')
+})
+
+
+////////////mongoDB/////
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USERS}:${process.env.DB_PASS}@cluster0.ruz4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+     
+    const gunsCollection = client.db('bulletDB').collection('bulletData')
+
+    //crud///
+
+     //products line data//
+     app.get('/bulletData',  async(req, res) => {
+        
+        const cursor = gunsCollection.find() 
+        const result = await cursor.toArray()
+        res.send(result);
+
+
+      })
+     //products line data// 
+      
+
+
+
+
+    //crud///
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+////////////mongoDB/////
+
+
+app.listen(port, () => {
+  console.log(`Bullet server port ${port}`)
+})
+
